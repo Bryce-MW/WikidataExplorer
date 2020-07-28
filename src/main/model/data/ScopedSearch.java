83c17bb4abcd617d5ab6abf923a5eff9bcecf7c8
Bryce Wilson <bryce@thenetworknerds.ca>
2020-07-27 21:53:36 -0700

    Line that was too long!

--- src/main/model/data/ScopedSearch.java
+++ src/main/model/data/ScopedSearch.java
@@ -2,7 +2,8 @@ package model.data;
 
 public class ScopedSearch {
     //TODO: Implement
-    private final Value item; // Item that this is scoped by, null for all items. Probably needs a data service for all items.
+    private final Value item; // Item that this is scoped by, null for all items. Probably needs a data service for all
+    // items.
     // private DatumQueryService queryService; TODO: Not needed yet
 
     public ScopedSearch() {
